c607f3641c9a984a8905746a3916a828b61870d1
DVM <dvmp@Douglass-MacBook-Pro.local>
2020-04-26 22:11:21 -0400

    Updates from Module 13

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,19 @@ import Order from './Order.js';
 import Inventory from './Inventory.js';
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    // To put into state: first take a copy of exising state by using objecct spread ...
+    const fishes = { ...this.state.fishes};
+    // Now we can add new fish to fishes object
+    fishes[`fish${Date.now()}`] = fish;
+    // Now we can set the new fishes object to state - takes copied fishes plus new fish
+    // Do not need to use fishes: fishes, just one as object and key are the same
+    this.setState({ fishes });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +24,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
